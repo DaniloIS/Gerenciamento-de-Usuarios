8b0ec4333933dc39aec8d246200d5ffe0ac6e880
DaniloIS <danilosantos-pg@hotmail.com>
2022-01-31 12:55:00 -0300

    Trabalhando com arquivos e datas

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -10,16 +10,84 @@ class UserController {
     }
 
     onSubmit() {
-        console.log(this.formEl)
 
         this.formEl.addEventListener("submit", event => {
 
             event.preventDefault();
 
-            this.addLine(this.getValues());
+            let btn = this.formEl.querySelector("[type=submit]");
+
+            btn.disabled = true;
+
+            let values = this.getValues();
+
+            this.getPhoto().then(
+                (content) => {
+
+                    values.photo = content;
+
+                    this.addLine(values);
+
+                    this.formEl.reset();
+
+                    btn.disabled = false;
+
+                },
+                (e) => {
+
+                    console.error(e);
+
+                }
+            );
+
+            this.getPhoto((content) => {
+
+                
+
+            });
+
         
         });
          
+    }
+
+    getPhoto() {
+
+        return new Promise((resolve, reject) => {
+
+            let fileReader = new FileReader();
+        
+            let elements = [...this.formEl.elements].filter(item=>{
+
+                if(item.name === "photo"){
+                    return item;
+                }
+
+            });
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = () => {
+
+                resolve(fileReader.result);
+
+            };
+
+            fileReader.onerror = (e) => {
+
+                reject(e);
+
+            }
+
+            if(file) {
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
+            }
+
+        });
+
+        
     }
 
     getValues() {
@@ -33,6 +101,10 @@ class UserController {
                 if(field.checked) {
                     user[field.name] = field.value;
                 }
+            } else if(field.name == "admin") {
+
+                user[field.name] = field.checked;
+
             } else {
                 
                 user[field.name] = field.value;
@@ -40,7 +112,7 @@ class UserController {
             }
         
         });
-        
+
         return new User(
             user.name,
             user.gender,
@@ -56,19 +128,21 @@ class UserController {
 
     addLine(dataUser) {
 
-        this.tableEl.innerHTML = `
-            <tr>
-                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+        let tr = document.createElement("tr");
+
+        tr.innerHTML = `
+                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
-                <td>${dataUser.birth}</td>
+                <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
+                <td>${dataUser.register}</td>
                 <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
-            </tr>
         `;
+
+        this.tableEl.appendChild(tr);
     
     }
 
