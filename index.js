05076cd80c0c0478961d53cc962ca832ff05e214
DaniloIS <danilosantos-pg@hotmail.com>
2022-01-31 10:49:53 -0300

    Trabalhando com Json e criando o model e controller

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-var name = document.querySelector("#exampleInputName");
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail1");
-var password = document.querySelector("#exampleInputPassword1");
-var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
-
-var fields = document.querySelectorAll("#form-user-create [name");
-
-fields.forEach(function(field, index){
-
-    console.log(field);
-
-});
+let userController = new UserController("form-user-create", "table-users");
